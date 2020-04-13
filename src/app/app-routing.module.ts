3b06c3b591c3a9e9ef8333086f4ccddd217bad46
Nikola Stoilov <ns@auxcode.com>
2020-04-13 23:14:39 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavigationComponent } from './core/navigation/navigation.component';
+import { ListComponent } from './memes/list/list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ListComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    data: {
+      isLogged: false
+    }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      isLogged: false
+    }
+  },
+  // {
+  //   path: '**',
+  //   component: NotFoundComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'meme-it-angular';
+  get isReady(): boolean {
+    return this.userUservice.currentUser !== undefined;
+  }
+
+  constructor(private userUservice: UserService) {
+
+  }
 }
