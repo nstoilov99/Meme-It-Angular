dec9a250eac1370017ba65cca921d863f3d87c65
Nikola Stoilov <ns@auxcode.com>
2020-04-13 23:31:27 +0300

    app module and interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { CoreModule } from './core/core.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { AppInterceptor } from './app-interceptor';
+import { MemeModule } from './memes/meme.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      RegisterComponent,
+      LoginComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      UserModule,
+      CoreModule,
+      MemeModule,
+      HttpClientModule,
+      NoopAnimationsModule,
+      FormsModule
+   ],
+   providers: [{
+      provide: HTTP_INTERCEPTORS,
+      useClass: AppInterceptor,
+      multi: true
+    }],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
