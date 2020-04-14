a424d81b81049d9a3e5af4086c984695af360a67
Nikola Stoilov <ns@auxcode.com>
2020-04-14 03:42:05 +0300

    usser routing and module

---
+++ src/app/user/user-routing.module.ts
@@ -0,0 +1,28 @@
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth.guard';
+import { ProfileComponent } from './profile/profile.component';
+import { DetailComponent } from '../memes/detail/detail.component';
+
+const routes: Routes = [
+  {
+    path: 'profile',
+    children: [{
+      path: '',
+      pathMatch: 'full',
+      component: ProfileComponent,
+    },
+    {
+      path: 'detail/:id',
+      component: DetailComponent,
+      canActivate: [AuthGuard],
+      data: {
+        shouldFetchMeme: true,
+        isLogged: true
+      }
+    }
+  ],
+  }
+  
+];
+
+export const UserRoutingModule = RouterModule.forChild(routes);
